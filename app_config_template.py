d7f4ec001c58f01a184a18bacc873316e8be3c39
JonahMonaghan <jonah.james.monaghan@gmail.com>
2023-10-27 13:35:50 -0400

    Added a app_config_template to maintain security while explaining the purpose of the file. Added .vscode to .gitignore

---
+++ app_config_template.py
@@ -0,0 +1,6 @@
+CLIENT_ID = "" #YOUR CLIENT ID HERE
+CLIENT_SECRET = "" #YOUR CLIENT SECRET HERE
+AUTHORITY = "https://login.microsoftonline.com/common"
+REDIRECT_PATH = "/get_token" #THIS CHANGES BASED ON YOUR AZURE SETTINGS
+SCOPE = ["Files.ReadWrite.All"]
+SESSION_TYPE = "filesystem"
